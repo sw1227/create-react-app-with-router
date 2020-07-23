a083566ba4e432a4ed2e71e3408e836bf5ead1d5
sw1227 <32032108+sw1227@users.noreply.github.com>
2020-07-23 15:35:51 +0900

    Add simple router usage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/hello", component: Hello },
+];
+
+
+function Hello() {
+  return <div>Hello, world!</div>;
+}
+
+
+function Home() {
   return (
-    <div>
-      Hello, world!
-    </div>
+    <ul>
+      {routes.map((r, i) => (
+        <li key={i}>
+          <Link to={r.path}>{r.component.name}</Link>
+        </li>
+      ))}
+    </ul>
   );
 }
 
-export default App;
+
+export default function AppRouter() {
+  return (
+    <Router>
+      {routes.map((r, i) => (
+        <Route path={r.path} exact component={r.component} key={i} />
+      ))}
+    </Router>
+  );
+}
